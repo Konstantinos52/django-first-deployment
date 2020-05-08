c27f44c6a554ef3f707184dd44909b948e525ab2
Konstantinos52 <65004798+Konstantinos52@users.noreply.github.com>
2020-05-08 16:17:21 +0200

    Update settings.py
    Change bootstrap3 to bootstrap4

--- lc/lc/settings.py
+++ lc/lc/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'bootstrap3',
+    'bootstrap4',
     'post.apps.PostConfig',
 ]
 
